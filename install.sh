14af561d8f628a3554f8570d120ac9aec27ae3d1
kevinjin420 <kevinjin0420@gmail.com>
2026-01-05 13:56:06 -0500

    zshrc

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DOTFILES_DIR="$HOME/dotfiles"
 
+ln -sf "$DOTFILES_DIR/zshrc" ~/.zshrc
+
 mkdir -p ~/.config/kitty
 ln -sf "$DOTFILES_DIR/config/kitty/kitty.conf" ~/.config/kitty/kitty.conf
