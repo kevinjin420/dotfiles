33ee0e68731e2b98a47f605e0b311224fc203b9b
kevinjin420 <kevinjin0420@gmail.com>
2026-05-06 20:11:10 -0700

    ansible play script, setup and rime scripts

---
+++ ansible.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Helper script to run Ansible playbooks
+# from the mrover-ros2 codebase
+
+set -euo pipefail
+
+if [ "$#" -le 0 ]; then
+    echo "Usage: $0 <playbook> <args>"
+    exit 1
+fi
+
+sudo -v # Ensure Ansible has sudo permission
+
+readonly ROOT_PATH=$(dirname "$0")
+ansible-playbook -i "localhost," -c local "${ROOT_PATH}"/ansible/"$1"
--- install.sh
+++ install.sh
@@ -10,3 +10,10 @@ ln -sf "$DOTFILES_DIR/always_forget.md" ~/always-forget.md
 
 mkdir -p ~/.config/kitty
 ln -sf "$DOTFILES_DIR/config/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+
+mkdir -p ~/.config/fcitx5/conf ~/.local/share/fcitx5/rime
+for f in config profile; do
+  [ ! -f ~/.config/fcitx5/$f ] && cp "$DOTFILES_DIR/config/fcitx5/$f" ~/.config/fcitx5/$f
+done
+[ ! -f ~/.config/fcitx5/conf/rime.conf ] && cp "$DOTFILES_DIR/config/fcitx5/conf/rime.conf" ~/.config/fcitx5/conf/rime.conf
+[ ! -f ~/.local/share/fcitx5/rime/default.custom.yaml ] && cp "$DOTFILES_DIR/local/share/fcitx5/rime/default.custom.yaml" ~/.local/share/fcitx5/rime/default.custom.yaml
